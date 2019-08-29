635ba84c05f15a16d099ac3a9f88d5cfecb98a60
Raul Sposito <raulsposito@gmail.com>
2019-08-29 19:28:45 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,14 +3,8 @@ class Post
   attr_accessor :title, :author
   
   def initialize(author)
-    @author = author
+    @title = title 
+    @author = author 
   end
   
-  
-  uncle_bob = Author.new("Uncle Bob")
-  hello_world = Post.new("Hello World!")
-  hello_world.author = uncle_bob
-  
 end
-
-
